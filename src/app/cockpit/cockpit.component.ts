11cd8ac1e65ec11a195418b00031ef38b8d673b5
Mark Allan Paul Sebastian <mapys@cloudstaff.com>
2020-01-16 08:01:36 +0800

    Lesson 65

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -8,25 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class CockpitComponent implements OnInit {
   newServerName = '';
   newServerContent = '';
-  
+
   constructor() { }
 
   ngOnInit() {
   }
 
   onAddServer() {
-    this.serverElements.push({
+    /*this.serverElements.push({
       type: 'server',
       name: this.newServerName,
       content: this.newServerContent
-    });
+    });*/
   }
 
   onAddBlueprint() {
-    this.serverElements.push({
+    /*this.serverElements.push({
       type: 'blueprint',
       name: this.newServerName,
       content: this.newServerContent
-    });
+    });*/
   }
 }
